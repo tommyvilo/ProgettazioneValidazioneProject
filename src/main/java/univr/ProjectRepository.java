28d8e6846c5f1b38275df0bde3757d5af6699977
nicozerman <nico.zerman@gmail.com>
2024-12-23 14:18:56 +0100

    tentativo fix n.2

--- src/main/java/univr/ProjectRepository.java
+++
@@ -1,7 +0,0 @@
-package univr;
-
-import org.springframework.data.repository.CrudRepository;
-
-public interface ProjectRepository extends CrudRepository<Project, Long>{
-    Iterable<Project> findProjectsByResearchersContains(Long researcher);
-}
--- src/main/java/univr/UserRepository.java
+++
@@ -1,8 +0,0 @@
-package univr;
-import univr.User.Utente;
-import org.springframework.data.repository.CrudRepository;
-
-public interface UserRepository extends CrudRepository<Utente, Long>{
-   Utente findByUsername(String username);
-   boolean existsByUsername(String username);
-}
